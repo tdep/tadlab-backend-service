caba7f131376cebd6db806a0ecbf373a564ad85b
Trevor DePew <tdepew@spotify.com>
2023-12-28 12:11:25 -0500

    adds db connection

--- src/main/java/com/tdep/tadlab/backendservice/Main.java
+++ src/main/java/com/tdep/tadlab/backendservice/Main.java
@@ -1,22 +1,27 @@
 package com.tdep.tadlab.backendservice;
 
-
-import com.tdep.tadlab.backendservice.controllers.SampleApplicationController;
 import java.net.URISyntaxException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Main {
-  private static Connection getConnection() throws URISyntaxException, SQLException {
-    String dbUrl = System.getenv("JDBC_DATABASE_URL");
-    return DriverManager.getConnection(dbUrl);
-  }
-
   public static void main(String[] args) throws Exception {
+    Connection connection = getConnection();
+
+    Statement stmt = connection.createStatement();
+    stmt.executeUpdate("DROP TABLE IF EXISTS ticks");
+    stmt.executeUpdate("CREATE TABLE ticks (tick timestamp)");
+    stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
+    ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
+    while (rs.next()) {
+      System.out.println("Read from DB: " + rs.getTimestamp("tick"));
+    }
 
     try{
       SpringApplication.run(Main.class, args);
@@ -24,5 +29,9 @@ public class Main {
       throw new RuntimeException(e.getMessage());
     }
 
+  }
+  private static Connection getConnection() throws URISyntaxException, SQLException {
+    String dbUrl = System.getenv("JDBC_DATABASE_URL");
+    return DriverManager.getConnection(dbUrl);
   }
 }
